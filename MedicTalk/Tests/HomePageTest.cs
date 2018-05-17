6047fa6655bb25877034899f560bef3b8b179217
unknown <Jordandecarheil1992@gmail.com>
2018-05-17 11:46:46 +1000

    Re added files again

---
+++ MedicTalk/Tests/HomePageTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MedicTalk;
+
+namespace MediTalk
+{
+	
+
+	[TestFixture]
+	class HomePageTest
+	{
+		public HomePage _homePage;
+		public Form1 _form1;
+		public Mysql_Connect _mysql;
+
+		[Test]
+		public void Should_allow_general_assistance_to_work()
+		{
+			_form1 = new MedicTalk.Form1();
+			_mysql = new MedicTalk.Mysql_Connect();
+			_homePage = new HomePage(_form1, _mysql);
+			_homePage.button10.PerformClick();
+			Assert.AreEqual(_homePage.result, true);
+			
+		}
+	}
+}
---
+++ MedicTalk/Tests/Requests_Handler_Unittest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MedicTalk;
+
+namespace MediTalk
+{
+    [TestFixture]
+    class Requests_Handler_Unittest
+    {
+        [Test]
+        public void Resident_Can_Add_New_Request()
+        {
+            Assert.IsTrue(Mysql_User_Handler.Login("admin", "admin"), "User should be able to log in, but can't");
+        }
+
+        [Test]
+        public void Staff_Can_View_Requests()
+        {
+            Assert.IsTrue(Mysql_User_Handler.Login("admin", "admin"), "User should be able to log in, but can't");
+        }
+
+        [Test]
+        public void Staff_Can_Mark_Requests_As_Complete()
+        {
+            Assert.IsTrue(Mysql_User_Handler.Login("admin", "admin"), "User should be able to log in, but can't");
+        }
+    }
+}
